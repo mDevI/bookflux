e5b20f360fc940485b9c7657e479561731e52d4d
Sergei Belonosov <2belonosov@gmail.com>
2018-09-03 09:03:42 +0300

    Initial commit.

---
+++ src/main/java/com/mdevi/bookflux/BookfluxApplication.java
@@ -0,0 +1,12 @@
+package com.mdevi.bookflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookfluxApplication.class, args);
+    }
+}
